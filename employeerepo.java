fb04e47f0c276ae7056e78d3baa14b2c53d936c1
Sahithi Ghanta <105491821+sahithi000@users.noreply.github.com>
2025-06-04 10:23:20 +0530

    Create employeerepo.java

---
+++ employeerepo.java
@@ -0,0 +1,3 @@
+import com.example.attendance.model.Employee; import
+org.springframework.data.jpa.repository.JpaRepository;
+public interface EmployeeRepository extends JpaRepository<Employee, Long> { }
