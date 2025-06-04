78d6c7ce39039de6cd0e97f6836c4d961b8bee21
Sahithi Ghanta <105491821+sahithi000@users.noreply.github.com>
2025-06-04 10:08:04 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,13 @@
+package com.example.library.model;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long userId;
+ private String name;
+ pri
