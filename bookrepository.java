5caccd1588711c3ff2dced4df49a5a7d6278e371
Sahithi Ghanta <105491821+sahithi000@users.noreply.github.com>
2025-06-04 10:09:13 +0530

    Create bookrepository.java

---
+++ bookrepository.java
@@ -0,0 +1,9 @@
+package com.example.library.repository;
+import com.example.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface BookRepository extends JpaRepository<Book, Long> {
+ List<Book> findByTitleContaining(String title);
+ List<Book> findByAuthorContaining(String author);
+ List<Book> findByCategory(String category);
+}
